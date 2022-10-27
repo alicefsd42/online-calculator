b7ab9a599f4668d7d34b4c89fbe978783ebc4aa6
Alicia Wonderland <aliciafsd42@gmail.com>
2022-10-27 19:01:47 -0400

    fixed send

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
-app.get('/hello', (req, res) =>
-  res.send('Hello World'))
+app.get('/add/:a/:b', (req, res) =>
+  res.send((parseInt(req.params.a) +
+            parseInt(req.params.b))
+            .toString()))
 app.listen(4000)
