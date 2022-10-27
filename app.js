59bc578788011f4704edce3a4001fd581c23da52
Alicia Wonderland <aliciafsd42@gmail.com>
2022-10-27 19:11:51 -0400

    implemented subtract feature

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 app.get('/add/:a/:b', (req, res) =>
   res.send((parseInt(req.params.a) +
-            parseInt(req.params.b))
-            .toString()))
+    parseInt(req.params.b))
+    .toString()))
+app.get('/subtract/:a/:b', (req, res) =>
+  res.send((parseInt(req.params.a) -
+    parseInt(req.params.b))
+    .toString()))
 app.listen(4000)
